609a7f77c959e89f7a48f58725a4dc98db3971e7
Jin Xu <xujincdl@cn.ibm.com>
2020-06-16 20:05:00 -0700

    Update to support Python 3

--- src/STATS_CARTPROD.py
+++ src/STATS_CARTPROD.py
@@ -3,13 +3,14 @@
 # *
 # * IBM SPSS Products: Statistics Common
 # *
-# * (C) Copyright IBM Corp. 1989, 2014
+# * (C) Copyright IBM Corp. 1989, 2020
 # *
 # * US Government Users Restricted Rights - Use, duplication or disclosure
-# * restricted by GSA ADP Schedule Contract with IBM Corp. 
-# ************************************************************************/
+# * restricted by GSA ADP Schedule Contract with IBM Corp.
+# ************************************************************************/"""Partial Least Squares Regression Module"""
 
-from __future__ import with_statement
+
+
 
 """STATS CARTPROD extension command"""
 
@@ -181,7 +182,7 @@ def dscheck(input2, var1, var2):
 def Run(args):
     """Execute the STATS CARTPROD extension command"""
 
-    args = args[args.keys()[0]]
+    args = args[list(args.keys())[0]]
     # debugging
     # makes debug apply only to the current thread
     #try:
@@ -215,7 +216,7 @@ def Run(args):
         def _(msg):
             return msg
     # A HELP subcommand overrides all else
-    if args.has_key("HELP"):
+    if "HELP" in args:
         #print helptext
         helper()
     else:
@@ -235,7 +236,7 @@ def helper():
     # webbrowser.open seems not to work well
     browser = webbrowser.get()
     if not browser.open_new(helpspec):
-        print("Help file not found:" + helpspec)
+        print(("Help file not found:" + helpspec))
         
 class NonProcPivotTable(object):
     """Accumulate an object that can be turned into a basic pivot table once a procedure state can be established"""
@@ -299,7 +300,7 @@ class NonProcPivotTable(object):
 def attributesFromDict(d):
     """build self attributes from a dictionary d."""
     self = d.pop('self')
-    for name, value in d.iteritems():
+    for name, value in d.items():
         setattr(self, name, value)
 
 def StartProcedure(procname, omsid):
